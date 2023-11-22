9f8acec70c691d87ad50f3c71c7cc35ce9d646fa
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2023-11-22 22:32:44 +0100

    Added simple CORS configuration and cleaned up properties

---
+++ src/main/java/com/example/backend/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.example.backend.config;
+
+import java.util.List;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+  @Value("${backend.cors.allowed-origins:http://localhost:3000}")
+  private List<String> allowedOrigins;
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(@NotNull CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins(allowedOrigins.toArray(String[]::new));
+      }
+    };
+  }
+}
